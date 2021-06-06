3a1de6f1b3942fd3c66b09cd5c4134c7f72b07f5
d3n5 <grosdenis@gmail.com>
2021-06-06 18:09:52 +0200

    update n2yo url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  BASE_URL: "https://images-api.nasa.gov",
-  N2YO_TLE_URL: 'https://www.n2yo.com/rest/v1/satellite/tle/'
-};
+  BASE_URL: 'https://images-api.nasa.gov',
+  N2YO_TLE_URL: 'https://api.n2yo.com/rest/v1/satellite/tle/',
+}
